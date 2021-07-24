f3fcaecdcff69d120ac578b38676771e2d0d83dd
leoneldc <leonelchavez2013@hotmail.com>
2021-07-24 11:31:23 -0600

    creación de la base de datos y agregado 5 tablas (torneo, estats de equipo, arbitros, clasificaciones de equipo, deportes)

---
+++ DeportesPro v1.0.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE MasterPro;
+
+USE MasterPro;
+
+-- -----------------------------------------------------
+-- Table `MasterPro`.`Torneos`
+-- -----------------------------------------------------
+CREATE TABLE torneos (
+  PK_codigo_torneo VARCHAR(5) NOT NULL,
+  PRIMARY KEY (PK_codigo_torneo)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+-- -----------------------------------------------------
+-- Table `MasterPro`.`Deportes`
+-- -----------------------------------------------------
+CREATE TABLE deportes (
+  PK_codigo_deporte VARCHAR(5) NOT NULL,
+  PRIMARY KEY (PK_codigo_deporte)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+-- -----------------------------------------------------
+-- Table `MasterPro`.`Estadisticas de Equipo`
+-- -----------------------------------------------------
+CREATE TABLE estats_Equipos (
+  PK_codigo_equipo VARCHAR(5) NOT NULL,
+  PRIMARY KEY (PK_codigo_equipo)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+-- -----------------------------------------------------
+-- Table `MasterPro`.`Arbitros`
+-- -----------------------------------------------------
+CREATE TABLE arbitros (
+  PK_codigo_arbitros VARCHAR(5) NOT NULL,
+  PRIMARY KEY (PK_codigo_arbitros)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+-- -----------------------------------------------------
+-- Table `MasterPro`.`Clasificaciones`
+-- -----------------------------------------------------
+CREATE TABLE clasificaciones (
+  PK_codigo_equipo VARCHAR(5) NOT NULL,
+  PRIMARY KEY (PK_codigo_equipo)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
