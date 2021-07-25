1197c3ba1306f7935a352ac874864d6d863f1a2c
leoneldc <leonelchavez2013@hotmail.com>
2021-07-24 20:58:05 -0600

    agregado contenedor prototipo

---
+++ DeportesPro/Vista/MDI.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace DeportesPro
+{
+    partial class MDI
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MDI));
+            this.navbar = new System.Windows.Forms.MenuStrip();
+            this.MenuMantenimientos = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntDeportes = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntAccionesDeportes = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntPenalizaciones = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntCategorias = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntDivisiones = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntJugadores = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntRoles = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntCampos = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntJornadas = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuProcesos = new System.Windows.Forms.ToolStripMenuItem();
+            this.prcsTorneos = new System.Windows.Forms.ToolStripMenuItem();
+            this.prcsEquipos = new System.Windows.Forms.ToolStripMenuItem();
+            this.prcsCalendario = new System.Windows.Forms.ToolStripMenuItem();
+            this.prcsPartidos = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuEstadisticas = new System.Windows.Forms.ToolStripMenuItem();
+            this.estadisticasJugadores = new System.Windows.Forms.ToolStripMenuItem();
+            this.estadisticasEquipo = new System.Windows.Forms.ToolStripMenuItem();
+            this.estadisticasTorneo = new System.Windows.Forms.ToolStripMenuItem();
+            this.clasificacionTorneos = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuSeguridad = new System.Windows.Forms.ToolStripMenuItem();
+            this.mntUsuarios = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajustoUsuario = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuCerrarSesion = new System.Windows.Forms.ToolStripMenuItem();
+            this.navbar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // navbar
+            // 
+            this.navbar.BackColor = System.Drawing.SystemColors.Window;
+            this.navbar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.navbar.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.navbar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuMantenimientos,
+            this.MenuProcesos,
+            this.MenuEstadisticas,
+            this.MenuSeguridad,
+            this.MenuCerrarSesion});
+            this.navbar.Location = new System.Drawing.Point(0, 0);
+            this.navbar.Name = "navbar";
+            this.navbar.Size = new System.Drawing.Size(1482, 31);
+            this.navbar.TabIndex = 0;
+            this.navbar.Text = "menuStrip1";
+            // 
+            // MenuMantenimientos
+            // 
+            this.MenuMantenimientos.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.MenuMantenimientos.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mntDeportes,
+            this.mntAccionesDeportes,
+            this.mntPenalizaciones,
+            this.mntCategorias,
+            this.mntDivisiones,
+            this.mntJugadores,
+            this.mntRoles,
+            this.mntCampos,
+            this.mntJornadas});
+            this.MenuMantenimientos.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MenuMantenimientos.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.MenuMantenimientos.Image = ((System.Drawing.Image)(resources.GetObject("MenuMantenimientos.Image")));
+            this.MenuMantenimientos.Name = "MenuMantenimientos";
+            this.MenuMantenimientos.Size = new System.Drawing.Size(191, 27);
+            this.MenuMantenimientos.Text = "MANTENIMIENTOS";
+            // 
+            // mntDeportes
+            // 
+            this.mntDeportes.Name = "mntDeportes";
+            this.mntDeportes.Size = new System.Drawing.Size(305, 28);
+            this.mntDeportes.Text = "Mant. Deportes";
+            // 
+            // mntAccionesDeportes
+            // 
+            this.mntAccionesDeportes.Name = "mntAccionesDeportes";
+            this.mntAccionesDeportes.Size = new System.Drawing.Size(305, 28);
+            this.mntAccionesDeportes.Text = "Mant. Acciones del Deporte";
+            // 
+            // mntPenalizaciones
+            // 
+            this.mntPenalizaciones.Name = "mntPenalizaciones";
+            this.mntPenalizaciones.Size = new System.Drawing.Size(305, 28);
+            this.mntPenalizaciones.Text = "Mant. Penalizaciones";
+            // 
+            // mntCategorias
+            // 
+            this.mntCategorias.Name = "mntCategorias";
+            this.mntCategorias.Size = new System.Drawing.Size(305, 28);
+            this.mntCategorias.Text = "Mant. Categorias";
+            // 
+            // mntDivisiones
+            // 
+            this.mntDivisiones.Name = "mntDivisiones";
+            this.mntDivisiones.Size = new System.Drawing.Size(305, 28);
+            this.mntDivisiones.Text = "Mant. Divisiones";
+            // 
+            // mntJugadores
+            // 
+            this.mntJugadores.Name = "mntJugadores";
+            this.mntJugadores.Size = new System.Drawing.Size(305, 28);
+            this.mntJugadores.Text = "Mant. Jugadores";
+            // 
+            // mntRoles
+            // 
+            this.mntRoles.Name = "mntRoles";
+            this.mntRoles.Size = new System.Drawing.Size(305, 28);
+            this.mntRoles.Text = "Mant. Roles";
+            // 
+            // mntCampos
+            // 
+            this.mntCampos.Name = "mntCampos";
+            this.mntCampos.Size = new System.Drawing.Size(305, 28);
+            this.mntCampos.Text = "Mant. Campos";
+            // 
+            // mntJornadas
+            // 
+            this.mntJornadas.Name = "mntJornadas";
+            this.mntJornadas.Size = new System.Drawing.Size(305, 28);
+            this.mntJornadas.Text = "Mant. Jornadas";
+            // 
+            // MenuProcesos
+            // 
+            this.MenuProcesos.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.MenuProcesos.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.prcsTorneos,
+            this.prcsEquipos,
+            this.prcsCalendario,
+            this.prcsPartidos});
+            this.MenuProcesos.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MenuProcesos.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.MenuProcesos.Image = ((System.Drawing.Image)(resources.GetObject("MenuProcesos.Image")));
+            this.MenuProcesos.Name = "MenuProcesos";
+            this.MenuProcesos.Size = new System.Drawing.Size(128, 27);
+            this.MenuProcesos.Text = "PROCESOS";
+            // 
+            // prcsTorneos
+            // 
+            this.prcsTorneos.Name = "prcsTorneos";
+            this.prcsTorneos.Size = new System.Drawing.Size(272, 28);
+            this.prcsTorneos.Text = "Creacion de Torneos";
+            // 
+            // prcsEquipos
+            // 
+            this.prcsEquipos.Name = "prcsEquipos";
+            this.prcsEquipos.Size = new System.Drawing.Size(272, 28);
+            this.prcsEquipos.Text = "Creacion de Equipos";
+            // 
+            // prcsCalendario
+            // 
+            this.prcsCalendario.Name = "prcsCalendario";
+            this.prcsCalendario.Size = new System.Drawing.Size(272, 28);
+            this.prcsCalendario.Text = "Creación de Calendario";
+            // 
+            // prcsPartidos
+            // 
+            this.prcsPartidos.Name = "prcsPartidos";
+            this.prcsPartidos.Size = new System.Drawing.Size(272, 28);
+            this.prcsPartidos.Text = "Creacion de Partidos";
+            // 
+            // MenuEstadisticas
+            // 
+            this.MenuEstadisticas.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.MenuEstadisticas.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.estadisticasJugadores,
+            this.estadisticasEquipo,
+            this.estadisticasTorneo,
+            this.clasificacionTorneos});
+            this.MenuEstadisticas.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MenuEstadisticas.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.MenuEstadisticas.Image = ((System.Drawing.Image)(resources.GetObject("MenuEstadisticas.Image")));
+            this.MenuEstadisticas.Name = "MenuEstadisticas";
+            this.MenuEstadisticas.Size = new System.Drawing.Size(152, 27);
+            this.MenuEstadisticas.Text = "ESTADISTICAS";
+            // 
+            // estadisticasJugadores
+            // 
+            this.estadisticasJugadores.Name = "estadisticasJugadores";
+            this.estadisticasJugadores.Size = new System.Drawing.Size(286, 28);
+            this.estadisticasJugadores.Text = "Estadisticas de Jugadores";
+            // 
+            // estadisticasEquipo
+            // 
+            this.estadisticasEquipo.Name = "estadisticasEquipo";
+            this.estadisticasEquipo.Size = new System.Drawing.Size(286, 28);
+            this.estadisticasEquipo.Text = "Estadisticas de Equipo";
+            // 
+            // estadisticasTorneo
+            // 
+            this.estadisticasTorneo.Name = "estadisticasTorneo";
+            this.estadisticasTorneo.Size = new System.Drawing.Size(286, 28);
+            this.estadisticasTorneo.Text = "Estadisticas de Torneo";
+            // 
+            // clasificacionTorneos
+            // 
+            this.clasificacionTorneos.Name = "clasificacionTorneos";
+            this.clasificacionTorneos.Size = new System.Drawing.Size(286, 28);
+            this.clasificacionTorneos.Text = "Clasificación";
+            // 
+            // MenuSeguridad
+            // 
+            this.MenuSeguridad.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.MenuSeguridad.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mntUsuarios,
+            this.ajustoUsuario});
+            this.MenuSeguridad.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MenuSeguridad.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.MenuSeguridad.Image = ((System.Drawing.Image)(resources.GetObject("MenuSeguridad.Image")));
+            this.MenuSeguridad.Name = "MenuSeguridad";
+            this.MenuSeguridad.Size = new System.Drawing.Size(136, 27);
+            this.MenuSeguridad.Text = "SEGURIDAD";
+            // 
+            // mntUsuarios
+            // 
+            this.mntUsuarios.Name = "mntUsuarios";
+            this.mntUsuarios.Size = new System.Drawing.Size(269, 28);
+            this.mntUsuarios.Text = "Mant. Usuario";
+            this.mntUsuarios.Click += new System.EventHandler(this.mntUsuarios_Click);
+            // 
+            // ajustoUsuario
+            // 
+            this.ajustoUsuario.Name = "ajustoUsuario";
+            this.ajustoUsuario.Size = new System.Drawing.Size(269, 28);
+            this.ajustoUsuario.Text = "Cambio de Contraseña";
+            // 
+            // MenuCerrarSesion
+            // 
+            this.MenuCerrarSesion.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.MenuCerrarSesion.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MenuCerrarSesion.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.MenuCerrarSesion.Image = ((System.Drawing.Image)(resources.GetObject("MenuCerrarSesion.Image")));
+            this.MenuCerrarSesion.Name = "MenuCerrarSesion";
+            this.MenuCerrarSesion.Size = new System.Drawing.Size(168, 27);
+            this.MenuCerrarSesion.Text = "CERRAR SESIÓN";
+            this.MenuCerrarSesion.Click += new System.EventHandler(this.MenuCerrarSesion_Click);
+            // 
+            // MDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1482, 653);
+            this.Controls.Add(this.navbar);
+            this.MainMenuStrip = this.navbar;
+            this.Name = "MDI";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MDI_FormClosing);
+            this.Load += new System.EventHandler(this.MDI_Load);
+            this.navbar.ResumeLayout(false);
+            this.navbar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip navbar;
+        private System.Windows.Forms.ToolStripMenuItem MenuMantenimientos;
+        private System.Windows.Forms.ToolStripMenuItem mntDeportes;
+        private System.Windows.Forms.ToolStripMenuItem mntAccionesDeportes;
+        private System.Windows.Forms.ToolStripMenuItem mntPenalizaciones;
+        private System.Windows.Forms.ToolStripMenuItem mntCategorias;
+        private System.Windows.Forms.ToolStripMenuItem mntDivisiones;
+        private System.Windows.Forms.ToolStripMenuItem mntJugadores;
+        private System.Windows.Forms.ToolStripMenuItem mntRoles;
+        private System.Windows.Forms.ToolStripMenuItem mntCampos;
+        private System.Windows.Forms.ToolStripMenuItem mntJornadas;
+        private System.Windows.Forms.ToolStripMenuItem MenuProcesos;
+        private System.Windows.Forms.ToolStripMenuItem prcsTorneos;
+        private System.Windows.Forms.ToolStripMenuItem prcsEquipos;
+        private System.Windows.Forms.ToolStripMenuItem prcsCalendario;
+        private System.Windows.Forms.ToolStripMenuItem prcsPartidos;
+        private System.Windows.Forms.ToolStripMenuItem MenuEstadisticas;
+        private System.Windows.Forms.ToolStripMenuItem estadisticasJugadores;
+        private System.Windows.Forms.ToolStripMenuItem estadisticasEquipo;
+        private System.Windows.Forms.ToolStripMenuItem estadisticasTorneo;
+        private System.Windows.Forms.ToolStripMenuItem clasificacionTorneos;
+        private System.Windows.Forms.ToolStripMenuItem MenuSeguridad;
+        private System.Windows.Forms.ToolStripMenuItem mntUsuarios;
+        private System.Windows.Forms.ToolStripMenuItem ajustoUsuario;
+        private System.Windows.Forms.ToolStripMenuItem MenuCerrarSesion;
+    }
+}
+
