54e38b9bead842646e73e18c86ecaa0ad2260e1b
jeffersondavila <jeffersondavila16@gmail.com>
2021-08-13 10:44:56 -0600

    Areglos en la BD

--- Polideportivo.sql
+++ Polideportivo.sql
@@ -1,18 +1,15 @@
-CREATE DATABASE MasterPro2;
+CREATE DATABASE MasterPro;
 
-USE MasterPro2;
+USE MasterPro;
 
--- -----------------------------------------------------
--- Table `MasterPro`.`Maestras`
--- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Table `MasterPro`.`Deportes`
 -- -----------------------------------------------------
 CREATE TABLE deportes (
   PK_codigo_deporte VARCHAR(5) NOT NULL,
   nombre_deporte VARCHAR(60) NOT NULL,
-  cantidad_min_jugadores INT(2) NOT NULL,
-  estatus_deporte TINYINT(1) NOT NULL,
+  cantidad_min_jugadores INT NOT NULL,
+  estatus_deporte TINYINT NOT NULL,
   PRIMARY KEY (PK_codigo_deporte)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
@@ -22,10 +19,10 @@ CREATE TABLE deportes (
 CREATE TABLE formatos (
   PK_codigo_formato VARCHAR(5) NOT NULL,
   nombre_formato VARCHAR(60) NOT NULL,
-  cantidad_min_equipos INT(2) NOT NULL,
-  cantidad_max_equipos INT(2) NOT NULL,
-  estatus_deporte TINYINT(1) NOT NULL,
-  PRIMARY KEY (PK_codigo_deporte)
+  cantidad_min_equipos INT NOT NULL,
+  cantidad_max_equipos INT NOT NULL,
+  estatus_deporte TINYINT NOT NULL,
+  PRIMARY KEY (PK_codigo_formato)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 -- -----------------------------------------------------
@@ -34,7 +31,7 @@ CREATE TABLE formatos (
 CREATE TABLE categorias (
   PK_codigo_categorias VARCHAR(5) NOT NULL,
   genero_categoria VARCHAR(60) NOT NULL,
-  estatus_categoria TINYINT(1) NOT NULL,
+  estatus_categoria TINYINT NOT NULL,
   PRIMARY KEY (PK_codigo_categorias)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
@@ -44,9 +41,9 @@ CREATE TABLE categorias (
 CREATE TABLE divisiones (
   PK_codigo_division VARCHAR(5) NOT NULL,
   nombre_division VARCHAR(60) NOT NULL,
-  edad_min_jugador INT(2) NOT NULL,
-  edad_max_jugador INT(2) NOT NULL,
-  estatus_division TINYINT(1) NOT NULL,
+  edad_min_jugador INT NOT NULL,
+  edad_max_jugador INT NOT NULL,
+  estatus_division TINYINT NOT NULL,
   PRIMARY KEY (PK_codigo_division)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
@@ -60,7 +57,7 @@ CREATE TABLE torneos (
   PK_codigo_division VARCHAR(5) NOT NULL,
   PK_codigo_formato VARCHAR(5) NOT NULL,
   nombre_torneo VARCHAR(60) NOT NULL,
-  estatus_torneo TINYINT(1) NOT NULL,
+  estatus_torneo TINYINT NOT NULL,
   PRIMARY KEY (
     PK_codigo_torneo,
     PK_codigo_deporte,
@@ -70,7 +67,7 @@ CREATE TABLE torneos (
   FOREIGN KEY (PK_codigo_deporte) REFERENCES deportes(PK_codigo_deporte),
   FOREIGN KEY (PK_codigo_categorias) REFERENCES categorias(PK_codigo_categorias),
   FOREIGN KEY (PK_codigo_division) REFERENCES divisiones(PK_codigo_division),
-  FOREIGN KEY (PK_codigo_formato) REFERENCES formatos(PK_codigo_formato),
+  FOREIGN KEY (PK_codigo_formato) REFERENCES formatos(PK_codigo_formato)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 -- -----------------------------------------------------
@@ -79,17 +76,18 @@ CREATE TABLE torneos (
 CREATE TABLE equipos (
   PK_codigo_equipo VARCHAR(5) NOT NULL,
   PK_codigo_deporte VARCHAR(5) NOT NULL,
-  PK_codigo_torneo VARCHAR(5) NOT NULL nombre_equipo VARCHAR(60) NOT NULL,
+  PK_codigo_torneo VARCHAR(5) NOT NULL,
+  nombre_equipo VARCHAR(60) NOT NULL,
   fecha_fundacion DATE NOT NULL,
-  cantidad_jugadores INT(2) NULL,
-  estatus_equipo TINYINT(1) NOT NULL,
+  cantidad_jugadores INT NULL,
+  estatus_equipo TINYINT NOT NULL,
   PRIMARY KEY (
     PK_codigo_equipo,
     PK_codigo_deporte,
     PK_codigo_torneo
   ),
   FOREIGN KEY (PK_codigo_deporte) REFERENCES deportes(PK_codigo_deporte),
-  FOREIGN KEY (PK_codigo_torneo) REFERENCES torneo(PK_codigo_torneo)
+  FOREIGN KEY (PK_codigo_torneo) REFERENCES torneos(PK_codigo_torneo)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 -- -----------------------------------------------------
@@ -99,7 +97,7 @@ CREATE TABLE roles(
   PK_codigo_rol VARCHAR(5) NOT NULL,
   PK_codigo_deporte VARCHAR(5) NOT NULL,
   nombre_rol VARCHAR(60) NOT NULL,
-  estatus_rol TINYINT(1) NOT NULL,
+  estatus_rol TINYINT NOT NULL,
   PRIMARY KEY (PK_codigo_rol, PK_codigo_deporte),
   FOREIGN KEY (PK_codigo_deporte) REFERENCES deportes(PK_codigo_deporte)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
@@ -112,15 +110,15 @@ CREATE TABLE jugadores (
   PK_codigo_rol VARCHAR(5) NOT NULL,
   PK_codigo_deporte VARCHAR(5) NOT NULL,
   PK_codigo_equipo VARCHAR(5) NOT NULL,
-  edad_jugador INT(2) NOT NULL,
+  edad_jugador INT NOT NULL,
   nombre_jugador VARCHAR(60) NOT NULL,
-  estatus_jugador TINYINT(1) NOT NULL,
+  estatus_jugador TINYINT NOT NULL,
   PRIMARY KEY (
     PK_codigo_jugador,
     PK_codigo_rol,
     PK_codigo_deporte
   ),
-  FOREIGN KEY (PK_codigo_rol) REFERENCES rol(PK_codigo_rol),
+  FOREIGN KEY (PK_codigo_rol) REFERENCES roles(PK_codigo_rol),
   FOREIGN KEY (PK_codigo_deporte) REFERENCES deportes(PK_codigo_deporte)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
@@ -132,12 +130,12 @@ CREATE TABLE arbitros (
   PK_codigo_deporte VARCHAR(5) NOT NULL,
   nombre_arbitro VARCHAR(60) NOT NULL,
   organizacion_arbitro VARCHAR(60) NOT NULL,
-  estatus_arbitros TINYINT(1) NOT NULL,
+  estatus_arbitros TINYINT NOT NULL,
   PRIMARY KEY (
     PK_codigo_arbitro,
-    PK_codigo_deporte,
+    PK_codigo_deporte
   ),
-  FOREIGN KEY (PK_codigo_deporte) REFERENCES deportes(PK_codigo_deporte),
+  FOREIGN KEY (PK_codigo_deporte) REFERENCES deportes(PK_codigo_deporte)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 -- -----------------------------------------------------
@@ -149,7 +147,7 @@ CREATE TABLE jornadas(
   nombre_jornada VARCHAR(60) NOT NULL,
   fecha_inicio DATE NOT NULL,
   fecha_fin DATE NOT NULL,
-  estatus_jornada TINYINT(1) NOT NULL,
+  estatus_jornada TINYINT NOT NULL,
   PRIMARY KEY (PK_codigo_jornada, PK_codigo_categorias),
   FOREIGN KEY (PK_codigo_categorias) REFERENCES categorias(PK_codigo_categorias)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
@@ -161,8 +159,8 @@ CREATE TABLE campos(
   PK_codigo_campo VARCHAR(5) NOT NULL,
   PK_codigo_deporte VARCHAR(5) NOT NULL,
   nombre_campo VARCHAR(60) NOT NULL,
-  estatus_campo TINYINT(1) NOT NULL,
-  PRIMARY KEY (PK_codigo_jornada, PK_codigo_deporte),
+  estatus_campo TINYINT NOT NULL,
+  PRIMARY KEY (PK_codigo_campo, PK_codigo_deporte),
   FOREIGN KEY (PK_codigo_deporte) REFERENCES deportes(PK_codigo_deporte)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
@@ -174,7 +172,7 @@ CREATE TABLE calendario (
   PK_codigo_deporte VARCHAR(5) NOT NULL,
   PK_codigo_jornada VARCHAR(5) NOT NULL,
   fecha DATE NOT NULL,
-  estatus_calendario TINYINT(1) NOT NULL,
+  estatus_calendario TINYINT NOT NULL,
   PRIMARY KEY (
     PK_codigo_calendario,
     PK_codigo_deporte,
@@ -191,7 +189,7 @@ CREATE TABLE acciones (
   PK_codigo_accion VARCHAR(5) NOT NULL,
   PK_codigo_deporte VARCHAR(5) NOT NULL,
   nombre_accion VARCHAR(60) NOT NULL,
-  estatus_accion TINYINT(1) NOT NULL,
+  estatus_accion TINYINT NOT NULL,
   PRIMARY KEY (PK_codigo_accion),
   FOREIGN KEY (PK_codigo_deporte) REFERENCES deportes(PK_codigo_deporte)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
@@ -203,10 +201,10 @@ CREATE TABLE sanciones (
   PK_codigo_sancion VARCHAR(5) NOT NULL,
   PK_codigo_accion VARCHAR(5) NOT NULL,
   nombre_sancion VARCHAR(60) NOT NULL,
-  partidos_sancion INT(2) NOT NULL,
-  estatus_accion TINYINT(1) NOT NULL,
+  partidos_sancion INT NOT NULL,
+  estatus_accion TINYINT NOT NULL,
   PRIMARY KEY (PK_codigo_sancion, PK_codigo_accion),
-  FOREIGN KEY (PK_codigo_accion) REFERENCES accion(PK_codigo_accion),
+  FOREIGN KEY (PK_codigo_accion) REFERENCES acciones(PK_codigo_accion)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 -- -----------------------------------------------------
@@ -217,7 +215,7 @@ CREATE TABLE login (
   nombre_usuario VARCHAR(60) NOT NULL,
   username VARCHAR(60) NOT NULL,
   password VARCHAR(100) NOT NULL,
-  estatus_usuario TINYINT(1) NOT NULL,
+  estatus_usuario TINYINT NOT NULL,
   PRIMARY KEY (PK_codigo_login)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
@@ -227,7 +225,7 @@ CREATE TABLE login (
 CREATE TABLE permisos (
   PK_codigo_permiso VARCHAR(5) NOT NULL,
   nombre_permiso VARCHAR(60) NOT NULL,
-  estatus_permiso TINYINT(1) NOT NULL,
+  estatus_permiso TINYINT NOT NULL,
   PRIMARY KEY (PK_codigo_permiso)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
@@ -237,7 +235,7 @@ CREATE TABLE permisos (
 CREATE TABLE aplicaciones (
   PK_codigo_aplicacion VARCHAR(5) NOT NULL,
   nombre_aplicacion VARCHAR(60) NOT NULL,
-  estatus_aplicacion TINYINT(1) NOT NULL,
+  estatus_aplicacion TINYINT NOT NULL,
   PRIMARY KEY (PK_codigo_aplicacion)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
@@ -259,9 +257,6 @@ CREATE TABLE autorizaciones (
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 -- -----------------------------------------------------
--- Table `MasterPro`.`TRANSACCIONALES`
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Table `MasterPro`.`Partido`
 -- -----------------------------------------------------
 CREATE TABLE partido (
@@ -272,12 +267,11 @@ CREATE TABLE partido (
   PK_codigo_campo VARCHAR(5) NOT NULL,
   hora_ini_partido TIME NOT NULL,
   PK_codigo_equipo_local VARCHAR(60) NOT NULL,
-  marcador_local INT(3) NOT NULL,
-  PK_codigo_equipo VARCHAR(60) NOT NULL,
-  marcador_visitante_visitante INT(3) NULL,
+  marcador_local INT NOT NULL,
+  PK_codigo_equipo_visitante VARCHAR(60) NOT NULL,
+  marcador_visitante INT NULL,
   PRIMARY KEY (PK_codigo_partido),
-  FOREIGN KEY (PK_codigo_calendario) REFERENCES calendario(PK_codigo_calendario),
-  FOREIGN KEY (PK_codigo_torneo) REFERENCES torneo(PK_codigo_torneo),
-  FOREIGN KEY (PK_codigo_equipo_local) REFERENCES equipo(PK_codigo_equipo_local),
-  FOREIGN KEY (marcador_visitante_visitante) REFERENCES equipo(marcador_visitante_visitante),
+  FOREIGN KEY (PK_codigo_torneo) REFERENCES torneos(PK_codigo_torneo),
+  FOREIGN KEY (PK_codigo_equipo_local) REFERENCES equipos(PK_codigo_equipo),
+  FOREIGN KEY (PK_codigo_equipo_visitante) REFERENCES equipos(PK_codigo_equipo)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
